f6ccac8bc3b355b2c15f3fd2f37fb1e2e4961155
Mirabel <amabomirabel@gmail.com>
2021-08-13 12:50:49 +0200

    task7

--- sample code.py
+++ sample code.py
@@ -1,3 +1,31 @@
 # sample code
 
 #Just testing
+
+# first_name ="malala"
+# last_name = "yousafzai"
+# first_name_cap =first_name.capitalize()
+# last_name_cap =last_name.capitalize()
+# print(first_name_cap)
+# print(last_name_cap)
+
+# Slicing task
+first_name ="malala"
+last_name = "yousafzai"
+note = "award: Nobel peace prize"
+first_name_cap =first_name.capitalize()
+last_name_cap =last_name.capitalize()
+print(first_name_cap)
+print(last_name_cap)
+award_location = note.find("award:" )
+print(award_location)
+award_text = note [7:]
+print(award_text)
+
+
+
+
+
+
+
+
